617eab1859756a34412fdb5033340e3af521cf21
Talha <mdtalha2008@gmail.com>
2022-09-03 23:54:35 +0600

    modify

--- script.js
+++ script.js
@@ -1,11 +1,24 @@
 const loadData = async () => {
-    const url = `https://openapi.programming-hero.com/api/news/categories`;
-    const res = await fetch(url);
-    const data = await res.json();
-    displayData(data.data.news_category);
-};
+    try {
+        const url = `https://openapi.programming-hero.com/api/news/categories`;
+        const res = await fetch(url);
+        const data = await res.json();
+        displayData(data.data.news_category);
+
+    }
+    catch {
+
+        console.log(error);
+    }
+
+}
+
+
 
 const displayData = (allCatagory) => {
+
+
+
 
     const catagories = document.getElementById('catagories');
 
@@ -13,32 +26,65 @@ const displayData = (allCatagory) => {
 
         const newDiv = document.createElement('div');
         newDiv.classList.add('row');
-        newDiv.innerHTML = ` <div onclick="loadNewsDetails(${catagory.category_id})" class="col text-primary" >${catagory.category_name}</div> `;
-        catagories.appendChild(newDiv);
-    });
-};
 
-const loadNewsDetails = category_id => {
-    toggleSpinner(true);
-    loadNewsDetail(category_id);
-};
+        newDiv.innerHTML = ` <div onclick="loadNewsDetail(${catagory.category_id})" class="col text-dark fw-bold" >${catagory.category_name}</div> `;
+        catagories.appendChild(newDiv)
+    });
+
+}
+
 
 const loadNewsDetail = async (category_id) => {
-    const url = `https://openapi.programming-hero.com/api/news/category/0${category_id} `;
-    const res = await fetch(url);
-    const data = await res.json();
-    displayNews(data.data);
+    try {
+        toggleSpinner(true);//spinner function parameter
+        const url = `https://openapi.programming-hero.com/api/news/category/0${category_id} `;
+        const res = await fetch(url);
+        const data = await res.json();
+        displayNews(data.data);
+    }
 
-};
-// Default news show
-loadNewsDetail(1);
+    catch {
+
+        console.log(error);
+    }
+
+}
 
 const displayNews = (allNews) => {
 
+    allNews.sort((a, b) => {
+        return b.total_view - a.total_view;
+    });
+
     const newsContainer = document.getElementById('news-container');
     newsContainer.innerHTML = ``;
+
+    const count = allNews.length;
+    const countNews = document.getElementById('count-news');
+    if (count !== 0) {
+        countNews.innerHTML = `
+    
+    <h3 class="text-primary">${count} items Found</h3>
+    `;
+
+    }
+
+    else {
+        countNews.innerHTML = `
+    
+        <h3 class="text-primary">No news Found</h3>
+        `;
+
+
+    }
+
+
+
+
+
     allNews.forEach(news => {
-        // console.log(news._id);
+
+
         const newsDiv = document.createElement('div');
         newsDiv.classList.add('col');
         newsDiv.innerHTML = `
@@ -53,76 +99,86 @@ const displayNews = (allNews) => {
                         <div class="d-flex">
                             <div><img class="image align-middle img-fluid rounded-circle" src="${news.author.img}"></div>
                             <div>
-                                <h6 class="p-2">${news.author.name}</h6>
+                                <h6 class="p-2">${news.author.name ? news.author.name : 'No data'}</h6>
                             </div>
                         </div>
                         <div class="d-flex">
                             <div><i class="p-2 fa-regular fa-eye"></i></div>
                             <div>
-                                <h4>${news.total_view}</h4>
+                                <h4>${news.total_view ? news.total_view : 'No data'}</h4>
                             </div>
                         </div>
 
                         <div>
-                        <!-- Button trigger modal -->
-   
-                        <button onclick="newsDetails('${news._id}')" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
-                            Open Modal
-                        </button>
+                        <button type="button" onclick="loadNewsDetails('${news._id}')" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                        Modal
+                    </button>
 
-                        </div>
                     </div>
-                    </div>`;
+                    </div>
+
+
+`;
         newsContainer.appendChild(newsDiv);
-    });
-    toggleSpinner(false);
-
-};
-loadData();
-
-const newsDetails = (id) => {
-    loadNewsModals(id);
-};
-
-const loadNewsModals = async (id) => {
-    const url = `https://openapi.programming-hero.com/api/news/${id}`;
-    const res = await fetch(url);
-    const data = await res.json();
-    displayModals(data);
-};
-
-const displayModals = (data) => {
-    const modal = document.getElementById('modal-id');
-    modal.innerHTML = `
-        <div class="modal-header">
-            <h5 class="modal-title" id="exampleModalLabel">${data.data[0].title}</h5>
-            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-          </div>
-          <div class="modal-body">
-          <img src="${data.data[0].image_url}" class="w-100">
-            <p>${data.data[0].details}</p>
-          </div>
-          <div class="modal-footer">
-            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-            <button type="button" class="btn btn-primary">Save changes</button>
-          </div>
-    `;
-
-
-    console.log(data.data[0].image_url);
-    console.log(data.data[0].title);
-    console.log(data.data[0].details);
-};
-
-loadNewsModals('0282e0e58a5c404fbd15261f11c2ab6a');
 
 
 
-const toggleSpinner = (isSpin) => {
-    const loaderSpinner = document.getElementById('loader');
-    if(isSpin) {
-        loaderSpinner.classList.remove('d-none');
-    } else {
-        loaderSpinner.classList.add('d-none');
+    })
+
+    toggleSpinner(false);////spinner function parameter
+
+}
+
+const loadNewsDetails = async news_id => {
+    try {
+        const url = `https://openapi.programming-hero.com/api/news/${news_id}`;
+
+        const res = await fetch(url);
+        const data = await res.json();
+        displayLoadNewsDetails(data.data[0]);
+
+
     }
-}; 
+
+    catch {
+
+        console.log(error);
+    }
+}
+
+const displayLoadNewsDetails = (newsDetail) => {
+
+    console.log(newsDetail);
+
+    const modalTitle = document.getElementById('exampleModalLabel');
+    modalTitle.innerHTML = `
+   
+       <h3>${newsDetail.author.name}</h3>
+       <p>Published date: ${newsDetail.author.published_date ? newsDetail.author.published_date : 'No Data'}</p>
+       `;
+
+    const modalDetails = document.getElementById('modal-body');
+
+    modalDetails.innerHTML = `
+       
+       <p> ${newsDetail.title ? newsDetail.title : 'No data'}</p>
+       
+       `;
+
+
+
+}
+
+const toggleSpinner = isLoading => {
+    const loaderSection = document.getElementById('spinner');
+    if (isLoading) {
+        loaderSection.classList.remove('d-none');
+    }
+
+    else {
+        loaderSection.classList.add('d-none');
+    }
+}
+
+
+loadData();
